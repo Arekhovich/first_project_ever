f99c7f0b1250f772bbd57f5c857218c2e4a84966
Arekhovich <e.orechovich@tut.by>
2020-12-08 23:29:38 +0300

    add likes for comments

--- manager/models.py
+++ manager/models.py
@@ -35,5 +35,18 @@ class Comment(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    likes_com = models.ManyToManyField(User, through="manager.LikeComment", related_name="liked_comments")
+
+class LikeComment(models.Model):
+    class Meta:
+        unique_together = ("user", "comment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_comment_table")
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="liked_comm_user_table")
+
+    def save(self, **kwargs):
+        try:
+            super().save(**kwargs)
+        except:
+            LikeComment.objects.get(user=self.user,comment=self.comment).delete()
 
 # Create your models here.
--- manager/views.py
+++ manager/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
 
-from manager.models import Book, LikeBookUser
+from manager.models import Book, LikeBookUser, LikeComment, Comment
 
 
 def hello(request, name="filipp", digit=None):
@@ -17,6 +17,7 @@ class MyPage(View):
     def get(self, request):
         context = {}
         context["books"] = Book.objects.prefetch_related("authors", "comments").annotate(count=Count("likes1"))
+        context["comments"] = Comment.objects.annotate(count=Count("likes_com"))
         return render(request, "index.html", context)
 
 
@@ -24,6 +25,13 @@ class AddLike(View):
     def get(self, request, id):
         if request.user.is_authenticated:
             LikeBookUser.objects.create(user=request.user, book_id=id)
+
+        return redirect("the-main-page")
+
+class AddCommentLike(View):
+    def get(self, request, id):
+        if request.user.is_authenticated:
+            LikeComment.objects.create(user=request.user, comment_id=id)
 
         return redirect("the-main-page")
 
