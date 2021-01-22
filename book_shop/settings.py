204381f79a1b18346235144a48035bd262ca1a38
lenovo <e.orechovich@tut.by>
2021-01-22 13:53:07 +0300

    fix settings

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -103,6 +103,10 @@ DATABASES = {
         'HOST': 'db'
     }
 }
+import sys
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    DATABASES['default']['NAME'] = ':memory:'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
